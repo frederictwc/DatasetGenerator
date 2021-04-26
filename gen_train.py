3d4f73c7ed3f3cd3caf727a0c581b5cd7fea4263
frederictwc <frederictwc@live.com>
2021-04-26 12:28:56 +0800

    added read and write

--- gen_train.py
+++ gen_train.py
@@ -1,11 +1,12 @@
 import glob
 
-path = 'dataset_2/'
+path = 'tuenmun_4/'
 
 def generate_train_and_val(image_path, txt_file):
-    with open(txt_file, 'w') as tf:
-        for jpg_file in glob.glob(image_path + '*.jpg'):
+    with open(txt_file, 'a+') as tf:
+        for jpg_file in glob.glob('tuenmun_4/raw/val/' + '*.jpg'):
+            print(jpg_file)
             tf.write(jpg_file + '\n')
 
-generate_train_and_val(path, path + 'train.txt')
+generate_train_and_val(path, path + 'val.txt')
 #generate_train_and_val(path + 'val_images/', path + 'val.txt')
