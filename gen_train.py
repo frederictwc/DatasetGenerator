34ac50eb2f2c11273e876bd5445e1d0cbd10a1c9
frederictwc <frederictwc@live.com>
2021-06-02 13:13:27 +0800

    improved

--- gen_train.py
+++ gen_train.py
@@ -1,12 +1,12 @@
 import glob
 
-path = 'tuenmun_4/'
+path = 'DIRT_4.1/'
 
 def generate_train_and_val(image_path, txt_file):
     with open(txt_file, 'a+') as tf:
-        for jpg_file in glob.glob('tuenmun_4/raw/val/' + '*.jpg'):
+        for jpg_file in glob.glob('DIRT_4.1/' + '*train*.jpg'):
             print(jpg_file)
             tf.write(jpg_file + '\n')
 
-generate_train_and_val(path, path + 'val.txt')
+generate_train_and_val(path, path + 'train.txt')
 #generate_train_and_val(path + 'val_images/', path + 'val.txt')
